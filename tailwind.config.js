b8a72808e6f79c33ac172a48f002ce6b018f382b
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2024-05-23 23:53:19 +0100

    refactor: deleted some unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   important: true,
   content: ['./src/**/*.{js,jsx,tsx,ts,mdx}'],
   theme: {
-    extend: {
-      colors: {
-        // we've added our own colour that maps to Payload's css variable so we can re-use the same theme styling!
-        error: 'var(--theme-error-400)',
-      },
-    },
+    extend: {},
   },
   darkMode: 'selector',
   plugins: [],
