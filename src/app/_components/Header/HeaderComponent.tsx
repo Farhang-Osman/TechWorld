8a6f40b443cf0aee5c7add2d5e455e53a8926d14
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2024-06-08 00:33:59 +0100

    feat: add noHeaderFooterUrls to HeaderComponent

--- src/app/_components/Header/HeaderComponent.tsx
+++ src/app/_components/Header/HeaderComponent.tsx
@@ -1,14 +1,24 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { Header } from '../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../constants'
 import { Gutter } from '../Gutter'
 import { HeaderNav } from './Nav'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
   return (
-    <nav>
+    <nav
+      className={['py-4', noHeaderFooterUrls.includes(pathname) && 'hidden']
+        .filter(Boolean)
+        .join(' ')}
+    >
       <Gutter>
         <Link href={'/'}>
           <Image src="/logo-black.svg" alt="logo" width={170} height={50} className="" />
