9df0944ec248b6dbeb505eb6c5f86d4fa12aceb7
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2024-06-12 23:47:33 +0100

    feat: modified RecoverPasswordForm

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import { Gutter } from '../../_components/Gutter'
+import { RenderParams } from '../../_components/RenderParams'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
-import classes from './index.module.scss'
-
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className="bg-[url('/assets/images/image-1.svg')] w-full h-full">
+      <div>
+        <Link href='/'>
+          <Image
+            src='/logo-black.svg'
+            alt='logo'
+            width={250}
+            height={23}
+            className=''
+          />
+        </Link>
+      </div>
+
+      <div className='bg-orange-100'>
+        <div>
+          <RenderParams />
+          <div className='flex'>
+            <Image
+              src='/assets/icons/arrow-left.svg'
+              alt='hand'
+              width={30}
+              height={30}
+            />
+            <p>back</p>
+          </div>
+          <div className='text-4xl font-bold'>
+            <p>Forgot Password</p>
+          </div>
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
